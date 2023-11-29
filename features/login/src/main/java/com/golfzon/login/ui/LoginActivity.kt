298dc2596f6f3d46a13aaa8e49a767ec68ac3296
DongJun-H <dongjun826@golfzon.com>
2023-11-29 18:01:44 +0900

    layout: Add login app bar

--- features/login/src/main/java/com/golfzon/login/ui/LoginActivity.kt
+++ features/login/src/main/java/com/golfzon/login/ui/LoginActivity.kt
@@ -2,6 +2,11 @@ package com.golfzon.login.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.golfzon.core_ui.extension.setOnDebounceClickListener
+import com.golfzon.login.R
 import com.golfzon.login.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,5 +17,28 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setAppbarVisibility()
+        setAppbarAction()
     }
+
+    private fun setAppbarAction() {
+        binding.btnLoginAppbarBack.setOnDebounceClickListener {
+            findNavController().navigateUp()
+        }
+    }
+    private fun setAppbarVisibility() {
+        findNavController().addOnDestinationChangedListener { _, destination, _ ->
+            binding.layoutLoginAppbar.visibility = when (destination.id) {
+                R.id.LoginFragment -> View.GONE
+                else -> View.VISIBLE
+            }
+        }
+    }
+
+    private fun findNavController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
+    }
+
 }
