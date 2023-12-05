2ab5e1355277d6e7bdd5bac2eafb3feeb501f8da
DongJun-H <dongjun826@golfzon.com>
2023-12-05 18:17:30 +0900

    layout: Add Recruit layout

--- features/recruit/src/main/java/com/golfzon/recruit/RecruitHomeFragment.kt
+++ features/recruit/src/main/java/com/golfzon/recruit/RecruitHomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.golfzon.core_ui.autoCleared
 import com.golfzon.recruit.databinding.FragmentRecruitHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,11 +26,24 @@ class RecruitHomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setBottomNavigationView()
     }
 
     private fun setDataBindingVariables() {
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
+        }
+    }
+
+    private fun setBottomNavigationView() {
+        with(binding.bottomNavigationRecruitpHome) {
+            setupWithNavController(findNavController())
+            selectedItemId = com.golfzon.core_ui.R.id.GroupFragment
+            itemIconTintList = null
+        }
+        binding.bottomNavigationRecruitpHome.menu.findItem(com.golfzon.core_ui.R.id.MatchingHomeFragment).setOnMenuItemClickListener {
+            (requireActivity() as RecruitActivity).navigateToGroup()
+            true
         }
     }
 }
