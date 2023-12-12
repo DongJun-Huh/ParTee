1fa78384b8d5e8cf01e422f72dcea01cccff8f2e
DongJun Huh <dongjun826@golfzon.com>
2023-12-12 14:10:20 +0900

    layout: Add displaying groups

---
+++ features/group/src/main/java/com/golfzon/group/GroupAdapter.kt
@@ -0,0 +1,74 @@
+package com.golfzon.group
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.golfzon.core_ui.databinding.ItemGroupBinding
+import com.golfzon.core_ui.extension.setOnDebounceClickListener
+import com.golfzon.domain.model.Group
+
+class GroupAdapter : ListAdapter<Group, GroupAdapter.GroupViewHolder>(diffCallback) {
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<Group>() {
+            override fun areItemsTheSame(
+                oldItem: Group,
+                newItem: Group
+            ) =
+                oldItem.groupUId == newItem.groupUId
+
+            override fun areContentsTheSame(
+                oldItem: Group,
+                newItem: Group
+            ): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View, group: Group)
+    }
+
+    private var listener: OnItemClickListener? = null
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroupViewHolder =
+        GroupViewHolder(
+            ItemGroupBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: GroupViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    override fun submitList(list: MutableList<Group>?) {
+        super.submitList(list?.let { ArrayList(it) })
+    }
+
+    inner class GroupViewHolder(private val binding: ItemGroupBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(group: Group) {
+            with(binding) {
+                root.setOnDebounceClickListener {
+                    listener?.onItemClick(it, group)
+                }
+
+                if (group.headCount != 0) {
+                    this.group = group
+                }
+                if (group.originalTeamsInfo.size == 2) {
+                    firstTeam = group.originalTeamsInfo[0]
+                    secondTeam = group.originalTeamsInfo[1]
+                }
+            }
+        }
+    }
+}
--- features/group/src/main/java/com/golfzon/group/GroupHomeFragment.kt
+++ features/group/src/main/java/com/golfzon/group/GroupHomeFragment.kt
@@ -5,22 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.golfzon.core_ui.GridSpacingItemDecoration
 import com.golfzon.core_ui.autoCleared
-import com.golfzon.core_ui.extension.setOnDebounceClickListener
+import com.golfzon.core_ui.dp
 import com.golfzon.group.databinding.FragmentGroupHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GroupHomeFragment : Fragment() {
-    private var binding by autoCleared<FragmentGroupHomeBinding>()
+    private var binding by autoCleared<FragmentGroupHomeBinding>{ onDestroyBindingView() }
+    private val groupViewModel by activityViewModels<GroupViewModel>()
+    private var groupAdapter: GroupAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentGroupHomeBinding.inflate(inflater, container, false)
+        getGroups()
         setDataBindingVariables()
         return binding.root
     }
@@ -28,7 +33,12 @@ class GroupHomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setBottomNavigationView()
-        tmpNavigate()
+        setGroupsAdapter()
+        observeGroups()
+    }
+
+    private fun onDestroyBindingView() {
+        groupAdapter = null
     }
 
     private fun setDataBindingVariables() {
@@ -54,20 +64,21 @@ class GroupHomeFragment : Fragment() {
             }
         }
     }
+    private fun getGroups() {
+        groupViewModel.getGroups()
+    }
 
-    // TODO 기능 구현 후 삭제
-    private fun tmpNavigate() {
-        binding.tempListComponent1.root.setOnDebounceClickListener {
-            findNavController().navigate(GroupHomeFragmentDirections.actionGroupHomeFragmentToGroupDetailFragment())
+    private fun setGroupsAdapter() {
+        groupAdapter = GroupAdapter()
+        with(binding.rvGroupHomeGroups) {
+            adapter = groupAdapter
+            addItemDecoration(GridSpacingItemDecoration(1, 12.dp))
         }
-        binding.tempListComponent2.root.setOnDebounceClickListener {
-            findNavController().navigate(GroupHomeFragmentDirections.actionGroupHomeFragmentToGroupDetailFragment())
-        }
-        binding.tempListComponent3.root.setOnDebounceClickListener {
-            findNavController().navigate(GroupHomeFragmentDirections.actionGroupHomeFragmentToGroupDetailFragment())
-        }
-        binding.tempListComponent4.root.setOnDebounceClickListener {
-            findNavController().navigate(GroupHomeFragmentDirections.actionGroupHomeFragmentToGroupDetailFragment())
+    }
+
+    private fun observeGroups() {
+        groupViewModel.groups.observe(viewLifecycleOwner) { groups ->
+            groupAdapter?.submitList(groups)
         }
     }
 }
