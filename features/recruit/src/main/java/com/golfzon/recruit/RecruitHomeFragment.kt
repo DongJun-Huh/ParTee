cd1b894833986976c1d911b84e448e2d79270ca4
DongJun Huh <dongjun826@golfzon.com>
2023-12-13 18:07:40 +0900

    layout: Add displaying recruits

--- features/recruit/src/main/java/com/golfzon/recruit/RecruitHomeFragment.kt
+++ features/recruit/src/main/java/com/golfzon/recruit/RecruitHomeFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.golfzon.core_ui.VerticalMarginItemDecoration
 import com.golfzon.core_ui.autoCleared
+import com.golfzon.core_ui.dp
+import com.golfzon.domain.model.Recruit
+import com.golfzon.domain.model.User
 import com.golfzon.recruit.databinding.FragmentRecruitHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RecruitHomeFragment : Fragment() {
-    private var binding by autoCleared<FragmentRecruitHomeBinding>()
+    private var binding by autoCleared<FragmentRecruitHomeBinding> { onDestroyBindingView() }
+    private val recruitViewModel by activityViewModels<RecruitViewModel>()
+    private var recruitPostAdapter: RecruitPostAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,18 +28,72 @@ class RecruitHomeFragment : Fragment() {
     ): View? {
         binding = FragmentRecruitHomeBinding.inflate(inflater, container, false)
         setDataBindingVariables()
+        getRecruits()
+        getRecruitsMembers()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setRecruitPostAdapter()
         setBottomNavigationView()
+        observeRecruitsMembers()
     }
+
+    private fun onDestroyBindingView() {
+        recruitPostAdapter = null
+    }
+
 
     private fun setDataBindingVariables() {
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
+            vm = recruitViewModel
         }
+    }
+
+    private fun getRecruits() {
+        recruitViewModel.getRecruits()
+    }
+
+    private fun getRecruitsMembers() {
+        recruitViewModel.recruits.observe(viewLifecycleOwner) { recruits ->
+            recruitViewModel.getRecruitsMembersInfo(
+                recruits.map { it.membersUId }
+            )
+        }
+    }
+
+    private fun observeRecruitsMembers() {
+        recruitViewModel.recruitsMembers.observe(viewLifecycleOwner) { recruitsMembers ->
+            // TODO 값을 받아온 후, 다시 재가공하는 부분 ViewModel로 이동
+            val recruitsDisplayInfo =
+                recruitViewModel.recruits.value!!.mapIndexed { recruitIndex, recruit ->
+                    Pair(
+                        recruit,
+                        recruitsMembers[recruitIndex].mapIndexed { index, pair ->
+                            pair.first
+                        }
+                    )
+                }.toMutableList()
+
+            recruitPostAdapter?.submitList(recruitsDisplayInfo)
+        }
+    }
+
+    private fun setRecruitPostAdapter() {
+        recruitPostAdapter = RecruitPostAdapter()
+        with(binding.rvRecruitPosts) {
+            adapter = recruitPostAdapter
+            addItemDecoration(VerticalMarginItemDecoration(20.dp))
+        }
+
+        recruitPostAdapter?.setOnItemClickListener(object :
+            RecruitPostAdapter.OnItemClickListener {
+            override fun onItemClick(view: View, recruitInfo: Pair<Recruit, List<User>>) {
+//                findNavController().navigate(  )
+            }
+        })
     }
 
     private fun setBottomNavigationView() {
---
+++ features/recruit/src/main/java/com/golfzon/recruit/RecruitPostAdapter.kt
@@ -0,0 +1,70 @@
+package com.golfzon.recruit
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.golfzon.core_ui.databinding.ItemRecruitPostBinding
+import com.golfzon.core_ui.extension.setOnDebounceClickListener
+import com.golfzon.domain.model.Recruit
+import com.golfzon.domain.model.User
+
+class RecruitPostAdapter :
+    ListAdapter<Pair<Recruit, List<User>>, RecruitPostAdapter.RecruitPostViewHolder>(diffCallback) {
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<Pair<Recruit, List<User>>>() {
+            override fun areItemsTheSame(
+                oldItem: Pair<Recruit, List<User>>,
+                newItem: Pair<Recruit, List<User>>
+            ) =
+                oldItem.first.recruitUId == newItem.first.recruitUId
+
+            override fun areContentsTheSame(
+                oldItem: Pair<Recruit, List<User>>,
+                newItem: Pair<Recruit, List<User>>
+            ): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View, recruitInfo: Pair<Recruit, List<User>>)
+    }
+
+    private var listener: OnItemClickListener? = null
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecruitPostViewHolder =
+        RecruitPostViewHolder(
+            ItemRecruitPostBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: RecruitPostViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    override fun submitList(list: MutableList<Pair<Recruit, List<User>>>?) {
+        super.submitList(list?.let { ArrayList(it) })
+    }
+
+    inner class RecruitPostViewHolder(private val binding: ItemRecruitPostBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(recruitInfo: Pair<Recruit, List<User>>) {
+            with(binding) {
+                root.setOnDebounceClickListener {
+                    listener?.onItemClick(it, recruitInfo)
+                }
+
+                binding.recruitDetail = recruitInfo.first
+            }
+        }
+    }
+}
