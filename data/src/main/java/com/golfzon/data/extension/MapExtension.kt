18b6cc8f63fe25ff3b9b1fa39b295e0c42adefc4
DongJun Huh <dongjun826@golfzon.com>
2023-12-15 16:23:01 +0900

    refactoring: Remove hardcoded part

--- data/src/main/java/com/golfzon/data/extension/MapExtension.kt
+++ data/src/main/java/com/golfzon/data/extension/MapExtension.kt
@@ -1,12 +1,10 @@
 package com.golfzon.data.extension
 
-import com.golfzon.data.common.TimestampDeserializer
 import com.golfzon.data.common.getTypeToken
 import com.golfzon.domain.model.Recruit
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonDeserializer
-import java.time.LocalDateTime
 
 inline fun <reified T> Map<String, Any>.toDataClass(): T {
     val gson = Gson()
@@ -14,9 +12,9 @@ inline fun <reified T> Map<String, Any>.toDataClass(): T {
     return gson.fromJson(json, getTypeToken<T>())
 }
 
-inline fun <reified T, V> Map<String, Any>.toDataClass(jsonDeserializer: JsonDeserializer<V>): T {
+inline fun <reified T, reified V> Map<String, Any>.toDataClass(jsonDeserializer: JsonDeserializer<V>): T {
     val gsonBuilder = GsonBuilder()
-    gsonBuilder.registerTypeAdapter(LocalDateTime::class.java, TimestampDeserializer())
+    gsonBuilder.registerTypeAdapter(V::class.java, jsonDeserializer)
     val gson = gsonBuilder.create()
     val json = gson.toJson(this)
     return gson.fromJson(json, getTypeToken<Recruit>())
